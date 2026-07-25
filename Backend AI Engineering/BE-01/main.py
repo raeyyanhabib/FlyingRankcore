9b25caa0e4ffd455c17b827cfd9b0c63517a59b3
Raeyyan Habib <raeyyanhabib12@gmail.com>
2026-07-25 13:12:27 +0500

    Stage 2 and 3 done

--- Backend AI Engineering/BE-01/main.py
+++ Backend AI Engineering/BE-01/main.py
@@ -1,7 +1,33 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 
 app = FastAPI()
 
+tasks = [
+    {"id": 1, "title": "Learn FastAPI", "done": False},
+    {"id": 2, "title": "Build a CRUD API", "done": False},
+    {"id": 3, "title": "Push to Github", "done": False}
+]
+
 @app.get("/")
 def read_root():
-    return {"message": "Hello, server is running"}
+    return {
+        "name": "Task API",
+        "version": "1.0",
+        "endpoints": ["/tasks"]
+    }
+
+@app.get("/health")
+def health_check():
+    return {"status": "ok"}
+
+@app.get("/tasks")
+def get_all_tasks():
+    return tasks 
+
+@app.get("/tasks/{id}")
+def get_task(id: int):
+    for task in tasks:
+        if task["id"] == id:
+            return task
+        else:
+            raise HTTPException(status_code=404, detail={"error": f"Task {id} not found"})
